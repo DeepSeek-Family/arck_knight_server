8eb7d8ea7575369cba47178c816acf96b7f396b5
iamrazzakk <rakibt23p@gmail.com>
2025-12-15 17:45:39 +0600

    refactor code

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,14 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { StatusCodes } from "http-status-codes";
 import { Morgan } from "./shared/morgan";
-import router from '../src/app/routes';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 const app = express();
-import path from 'path';
+import path from "path";
+import router from "./app/routes";
 
 // morgan
 app.use(Morgan.successHandler);
 app.use(Morgan.errorHandler);
-
 
 //body parser
 app.use(cors());
@@ -18,8 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //file retrieve
-app.use(express.static('uploads'));
-app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(express.static("uploads"));
+app.use(express.static(path.join(__dirname, "app/public")));
 
 // Session middleware (must be before passport initialization)
 // Initialize Passport
@@ -27,27 +26,27 @@ app.use(express.static(path.join(__dirname, 'app/public')));
 // app.use(passport.session());
 
 //router
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'app/public', 'index.html'));
+  res.sendFile(path.join(__dirname, "app/public", "index.html"));
 });
 
 //global error handle
 app.use(globalErrorHandler);
 
 // handle not found route
-app.use((req: Request, res: Response)=>{
-    res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-        message: "Not Found",
-        errorMessages: [
-            {
-                path: req.originalUrl,
-                message: "API DOESN'T EXIST"
-            }
-        ]
-    })
+app.use((req: Request, res: Response) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API DOESN'T EXIST",
+      },
+    ],
+  });
 });
 
 export default app;
